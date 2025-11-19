717a25993549e8eead57f6179072e9e05a2163eb
desny <trsadesny@gmail.com>
2025-11-19 10:12:01 +0530

    react

--- MYAPP/src/App.jsx
+++ MYAPP/src/App.jsx
@@ -5,7 +5,7 @@ import Navbar from './components/navbar.jsx';
 function App(){
     return(
       <div className="App">
-        <Navbar />
+        <Navbar name="Desny" />
         <h1>Welcome to myReact App</h1>
         <p>This is a simple React application</p>
       </div>
--- MYAPP/src/components/navbar.jsx
+++ MYAPP/src/components/navbar.jsx
@@ -1,3 +1,18 @@
 import React from "react";
 import './nav.css'
 
+function Navbar(props){
+    return(
+        <nav className="navbar">
+            <h2>{props.name}</h2>
+            <h2 className="navbar-title"></h2>
+            <ul className="navbar-links">
+                <li><a href="">Home</a></li>
+                <li><a href="">About</a></li>
+                <li><a href="">Contact</a></li>
+            </ul>
+        </nav>
+    );
+}
+
+export default Navbar;
